5d9a30305762ddb91062592ede53b142cc592656
Luciano Portales <lportales655@gmail.com>
2022-11-12 12:21:56 -0300

    validate stars

--- src/companies/dtos/valoration.dto.ts
+++ src/companies/dtos/valoration.dto.ts
@@ -1,7 +1,15 @@
 import { ApiProperty, PartialType } from '@nestjs/swagger';
+import { IsNumber, IsPositive } from 'class-validator';
 
 export class CreateValorationDto {
-  @ApiProperty()
+
+  // VALIDATE THAT VALUES IS BETWEEN 1 AND 5
+  @IsNumber()
+  @IsPositive()
+  @ApiProperty({
+    description: 'Puntuación de la empresa',
+    example: 5,
+  })
   valoration: number;
 }
 
