381cad4ed03bb681b58dbca269f36c757d4f28a6
Luciano Portales <lportales655@gmail.com>
2022-11-12 12:35:17 -0300

    validation login

--- src/auth/dtos/auth.dto.ts
+++ src/auth/dtos/auth.dto.ts
@@ -1,32 +1,44 @@
 import { ApiProperty, PartialType } from '@nestjs/swagger';
-import { IsMongoId, IsString } from 'class-validator';
+import { IsEmail, IsMongoId, IsString } from 'class-validator';
 
 export class AuthDTO {
   @ApiProperty()
-  @IsString()
+  @IsString({
+    message: 'El nombre de usuario debe ser una cadena de texto',
+  })
+  @IsEmail('El nombre de usuario debe ser un correo electrónico válido')
   email: string;
 
   @ApiProperty()
-  @IsString()
+  @IsString({
+    message: 'La contraseña debe ser un texto',
+  })
   password: string;
 
   @ApiProperty()
-  @IsString()
+  @IsString({ message: 'La contraseña debe ser un texto' })
   confirmPassword: string;
 
   @ApiProperty()
-  @IsMongoId()
+  @IsMongoId({
+    message: 'El ID del usuario debe ser un ID de MongoDB válido',
+  })
   @IsString()
   user: string;
 }
 
 export class LoginDTO {
   @ApiProperty()
-  @IsString()
+  @IsString({
+    message: 'El email de usuario debe ser una cadena de texto',
+  })
+  @IsEmail('El email de usuario debe ser un correo electrónico válido')
   email: string;
 
   @ApiProperty()
-  @IsString()
+  @IsString({
+    message: 'La contraseña debe ser un texto',
+  })
   password: string;
 }
 
